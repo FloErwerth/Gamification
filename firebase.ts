2e912414adaeea245f66cbbdf2f50c0892a9dbcb
Florian Erwerth <erwerthflorian@outlook.de>
2023-03-04 19:06:22 +0100

    add support for storing and reading data from database

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth";
 import {initializeApp} from "firebase/app";
-
+import {collection, getDoc, doc, updateDoc,getFirestore, setDoc, arrayUnion, FirestoreDataConverter} from "firebase/firestore";
+import {StatsProps} from "./src/components/stats/Stats";
 const firebaseApp = initializeApp({
    apiKey: "AIzaSyDFK3fGAEFWRpdAhwC5FPE5beGcNDzAMXk",
    authDomain: "gamification-a0ec5.firebaseapp.com",
@@ -16,3 +17,26 @@ export const Signup = (email: string, password: string) => createUserWithEmailAn
 export const Signin = (email: string, password: string) => signInWithEmailAndPassword(Auth, email, password);
 export const SignOut = () => signOut(Auth);
 export default firebaseApp;
+
+const firebaseDB = getFirestore(firebaseApp);
+const getUserCollection = () => collection(firebaseDB, "Nutzerdaten");
+const getSpecificUserCollection = (uid: string) => collection(firebaseDB, "Nutzerdaten", `${uid}/activities`);
+
+export const addFirebaseUser = async (userId: string) => {
+   const userCollection = getUserCollection();
+   await setDoc(doc(userCollection, userId), {activities: []}, {merge: true});
+}
+const getActivitiesRef = (uid: string) => doc(firebaseDB, "Nutzerdaten", uid);
+export const getStoredActivities = async (userId: string): Promise<StatsProps[]> => {
+   const doc = await getDoc(getActivitiesRef(userId));
+   if(doc.exists()) {
+      return doc.data().activities as StatsProps[];
+   }
+   else return []
+}
+
+export const addActivityInDatabase = async (uid: string, data: StatsProps) => {
+   const activityRef = getActivitiesRef(uid);
+   await updateDoc(activityRef, {activities: arrayUnion({...data})});
+}
+
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,54 +1,77 @@
-import {useCallback, useState} from "react";
-import {getClasses} from "../../utils/styleUtils";
-import {loginStyles} from "./loginStyles";
-import {Input} from "../../components/basicComponents/Input/Input";
-import {Button} from "../../components/basicComponents/Button/Button";
-import {useNavigate} from "react-router-dom";
-import {Pages} from "../../types/pages";
-import {FormWrapper} from "../../components/basicComponents/FormWrapper/FormWrapper";
-import {setEmailAction, setLoggedIn} from "../../store/authentication/authActions";
-import {useAppDispatch} from "../../store/store";
-import {Signin} from "../../../firebase";
+import { useCallback, useState } from "react";
+import { getClasses } from "../../utils/styleUtils";
+import { loginStyles } from "./loginStyles";
+import { Input } from "../../components/basicComponents/Input/Input";
+import { Button } from "../../components/basicComponents/Button/Button";
+import { useNavigate } from "react-router-dom";
+import { Pages } from "../../types/pages";
+import { FormWrapper } from "../../components/basicComponents/FormWrapper/FormWrapper";
+import {
+  setEmailAction,
+  setLoggedIn,
+  setUserId,
+} from "../../store/authentication/authActions";
+import { useAppDispatch } from "../../store/store";
+import { getStoredActivities, Signin } from "../../../firebase";
+import { setActivities } from "../../store/activities/acitivityActions";
 
 const cssClasses = getClasses(loginStyles);
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-   const navigate = useNavigate();
-   const dispatch = useAppDispatch();
+  const handleLogin = useCallback(async () => {
+    const user = await Signin(email, password);
+    const loggedIn = Boolean(user.user.email);
+    dispatch(setLoggedIn(loggedIn));
+    dispatch(setUserId(user.user.uid));
 
-   const handleLogin = useCallback(async () => {
-      const user = await Signin(email, password)
-      const loggedIn = Boolean(user.user.email);
-      dispatch(setLoggedIn(loggedIn));
-      if (Boolean(user.user.email) && user.user.email) {
-         dispatch(setEmailAction(user.user.email))
-         navigate(Pages.OVERVIEW);
-      }
-   }, [email, password])
+    const storedActivities = await getStoredActivities(user.user.uid);
+    if (storedActivities) {
+      dispatch(setActivities(storedActivities));
+    }
 
+    if (Boolean(user.user.email) && user.user.email) {
+      dispatch(setEmailAction(user.user.email));
+      navigate(Pages.OVERVIEW);
+    }
+  }, [email, password]);
 
-   return <FormWrapper>
+  return (
+    <FormWrapper>
       <div className={cssClasses.title}>Login</div>
       <div className={cssClasses.inputWrapper}>
-         <Input onChange={(value) => setEmail(value)} value={email}
-                label={"Email"}
-                id="email"
-                type={"email"}/>
+        <Input
+          onChange={(value) => setEmail(value)}
+          value={email}
+          label={"Email"}
+          id="email"
+          type={"email"}
+        />
 
-         <Input onChange={(value) => setPassword(value)} value={password}
-                label={"Password"}
-                id="password"
-                type={"password"}/>
+        <Input
+          onChange={(value) => setPassword(value)}
+          value={password}
+          label={"Password"}
+          id="password"
+          type={"password"}
+        />
       </div>
-      <div className={cssClasses.buttonWrapper}><Button className={cssClasses.loginButton}
-                                                        onClick={handleLogin}>Login</Button>
-         <button className={cssClasses.registerLink} onClick={() => navigate(Pages.REGISTER)}>Have no account yet? Click
-            here to create one
-         </button>
+      <div className={cssClasses.buttonWrapper}>
+        <Button className={cssClasses.loginButton} onClick={handleLogin}>
+          Login
+        </Button>
+        <button
+          className={cssClasses.registerLink}
+          onClick={() => navigate(Pages.REGISTER)}
+        >
+          Have no account yet? Click here to create one
+        </button>
       </div>
-   </FormWrapper>
-}
+    </FormWrapper>
+  );
+};
